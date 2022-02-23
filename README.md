76fb937f0ac1c33d2616efa33c45dad0a68e7984
Tony <tony@fastmail.in>
2022-02-23 18:40:45 +0300

    contract from vetoken-finance

---
+++ README.md
@@ -0,0 +1,11 @@
+# vetoken-contracts
+
+`npm run fork-mainnet`
+
+# for Curve
+`npm run deploy-local-curve`
+
+# for Pickle
+`npm run deploy-local-pickle`
+
+`npm run test-deposit-pickle`
---
+++ migrations/5_deploy_vetoken.js
@@ -0,0 +1,11 @@
+const veToken = artifacts.require("veToken");
+const { constants } = require("@openzeppelin/test-helpers");
+const addContract = require("./helper/addContracts");
+
+module.exports = async function (deployer, network, accounts) {
+  let pickleVoterProxy = "0x05A7Ebd3b20A2b0742FdFDe8BA79F6D22Ea9C351";
+
+  // vetoken
+  await deployer.deploy(veToken, constants.ZERO_ADDRESS, pickleVoterProxy);
+  let vetoken = await veToken.deployed();
+};
