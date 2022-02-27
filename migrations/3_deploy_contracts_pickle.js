05df2f4d917472e2fc9392144685521c133c90d2
amrshaban2005 <amr_shaban2005@yahoo.com>
2022-02-27 23:28:28 +0300

    vetoken minter

--- migrations/3_deploy_contracts_pickle.js
+++ migrations/3_deploy_contracts_pickle.js
@@ -11,11 +11,11 @@ const Booster = artifacts.require("PickleBooster");
 const TokenFactory = artifacts.require("PickleTokenFactory");
 const vetokenRewardPool = artifacts.require("PickleVtRewardPool");
 const PoolManager = artifacts.require("PicklePoolManager");
-const ForceSend = artifacts.require("ForceSend");
 const SmartWalletWhitelist = artifacts.require("SmartWalletWhitelist");
 const dillABI = require("./helper/DILL.json");
 const IExchange = artifacts.require("IExchange");
 const IERC20 = artifacts.require("IERC20");
+const veTokenMinter = artifacts.require("veTokenMinter");
 
 module.exports = async function (deployer, network, accounts) {
   let pickle = await IERC20.at("0x429881672B9AE42b8EbA0E26cD9C73711b891Ca5");
@@ -23,11 +23,12 @@ module.exports = async function (deployer, network, accounts) {
   let voterProxyAddress = "0x05A7Ebd3b20A2b0742FdFDe8BA79F6D22Ea9C351";
   let voterProxyOwner = "0x30a8609c9d3f4a9ee8ebd556388c6d8479af77d1";
   let pickleUser = "0xF8dB00cDdEEDd6BEA28dfF88F6BFb1B531A6cBc9";
+  let veTokenAddress = "0x1F209ed40DD77183e9B69c72106F043e0B51bf24";
+  let vetokenOperator = "0xa2a379a34cc30c69ab5597bb1c4b6c5c8b23d87e";
   let admin = accounts[0];
 
-  // send ether to the checker admin
-  let forceSend = await ForceSend.new();
-  await forceSend.go(voterProxyOwner, { value: ether("10"), from: admin });
+  web3.eth.sendTransaction({ from: admin, to: voterProxyOwner, value: web3.utils.toWei("10") });
+  web3.eth.sendTransaction({ from: admin, to: vetokenOperator, value: web3.utils.toWei("10") });
 
   console.log("deploying from: " + admin);
 
@@ -40,14 +41,20 @@ module.exports = async function (deployer, network, accounts) {
   await pickle.transfer(voter.address, web3.utils.toWei("1000"), { from: admin });
 
   // vetoken
-  await deployer.deploy(veToken);
-  let vetoken = await veToken.deployed();
+
+  await deployer.deploy(veTokenMinter);
+  let vetokenMinter = await veTokenMinter.deployed();
+  //mint vetoke to minter contract
+  const vetoken = await veToken.at(veTokenAddress);
+  await vetoken.mint(vetokenMinter.address, web3.utils.toWei("30000000"), { from: vetokenOperator });
+
   addContract("system", "pickle", pickle.address);
   addContract("system", "picklevoterProxy", voter.address);
-  addContract("system", "vetoken", vetoken.address);
+  addContract("system", "vetoken", veTokenAddress);
+  addContract("system", "vetokenMinter", vetokenMinter.address);
 
   // booster
-  await deployer.deploy(Booster, voter.address, vetoken.address);
+  await deployer.deploy(Booster, voter.address, vetokenMinter.address);
   const booster = await Booster.deployed();
   addContract("system", "pickleBooster", booster.address);
   await voter.setOperator(booster.address, { from: voterProxyOwner });
@@ -84,7 +91,7 @@ module.exports = async function (deployer, network, accounts) {
   // vetokenRewardPool
   await deployer.deploy(
     vetokenRewardPool,
-    vetoken.address,
+    veTokenAddress,
     pickle.address,
     pickleDepositor.address,
     vtpickleTokenRewards.address,
